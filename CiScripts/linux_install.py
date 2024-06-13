c0482b321e95f921290dc8abdc95f37fa75fd02a
stgatilov <stgatilov@49c82d7f-2e2a-0410-a16f-ae8f201b507f>
2024-06-13 18:20:36 +0000

    asdf
    git-svn-id: http://svn.thedarkmod.com/svn/darkmod_src/branches/appveyor@10786 49c82d7f-2e2a-0410-a16f-ae8f201b507f

--- CiScripts/linux_install.py
+++ CiScripts/linux_install.py
@@ -14,5 +14,6 @@ if 'windows' not in sysname:
     os.system('sudo dpkg --add-architecture i386')
     os.system('sudo apt-get update')
     os.system('sudo apt-get -y install gcc-multilib g++-multilib')
-    os.system('sudo apt-get -y install mesa-common-dev:i386 libglu1-mesa-dev:i386 libglu1-mesa:i386 libgl1-mesa-dev:i386 libgl1:i386 libgl1-mesa-glx:i386 libglvnd-dev:i386')
+#    os.system('sudo apt-get -y install mesa-common-dev:i386 libglu1-mesa-dev:i386 libglu1-mesa:i386 libgl1-mesa-dev:i386 libgl1:i386 libgl1-mesa-glx:i386 libglvnd-dev:i386')
     os.system('sudo apt-get -y install libxcb-*-dev:i386 libx11-xcb-dev:i386 libxxf86vm-dev:i386 libxext-dev:i386')
+    os.system('sudo apt-get -y install libfontenc-dev:i386 libice-dev:i386 libxt-dev:i386 libxaw7-dev:i386 libxcursor-dev:i386 libxfixes-dev:i386 libxdamage-dev:i386 libxinerama-dev:i386 libxkbfile-dev:i386 libxmu-dev:i386 libxmuu-dev:i386 libxpm-dev:i386 libxrandr-dev:i386 libxrender-dev:i386 libxss-dev:i386 libxv-dev:i386 libxcb-cursor-dev:i386 libsm-dev:i386 uuid-dev:i386')
