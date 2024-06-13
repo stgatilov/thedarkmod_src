05fe08dd06982bda4339a52f99ad35fae4d15103
stgatilov <stgatilov@49c82d7f-2e2a-0410-a16f-ae8f201b507f>
2024-06-13 17:04:02 +0000

    #6253. New branch for Appveyor hacking.
    git-svn-id: http://svn.thedarkmod.com/svn/darkmod_src/branches/appveyor@10783 49c82d7f-2e2a-0410-a16f-ae8f201b507f

---
+++ CiScripts/linux_install.py
@@ -0,0 +1,18 @@
+import os, platform, sys
+
+def execute(cmd):
+    print("CMD: " + cmd)
+    assert os.system(cmd) == 0
+
+
+sysname = platform.system().lower()
+if 'windows' not in sysname:
+    # this is enough for 64 bit 
+    os.system('sudo apt-get update')
+    os.system('sudo apt-get -y install mesa-common-dev libglu1-mesa-dev xorg-dev libxcb-*-dev libx11-xcb-dev libxxf86vm-dev libxext-dev')
+    # for 32 bit
+    os.system('sudo dpkg --add-architecture i386')
+    os.system('sudo apt-get update')
+    os.system('sudo apt-get -y install gcc-multilib g++-multilib')
+    os.system('sudo apt-get -y install mesa-common-dev:i386 libglu1-mesa-dev:i386 libglu1-mesa:i386 libgl1-mesa-dev:i386 libgl1:i386 libgl1-mesa-glx:i386 libglvnd-dev:i386')
+    os.system('sudo apt-get -y install libxcb-*-dev:i386 libx11-xcb-dev:i386 libxxf86vm-dev:i386 libxext-dev:i386')
